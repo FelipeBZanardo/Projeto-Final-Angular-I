bf8d64432339c27effe070d469d626b6753fbd08
Felipe Zanardo <felipebueno6@hotmail.com>
2023-06-23 15:23:27 -0300

    refactor: mudança no html principal com adição do component experiences-item

--- curriculo-angular/src/app/app.module.ts
+++ curriculo-angular/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { SkillsComponent } from './components/skills/skills.component';
 import { SkillItemComponent } from './components/skills/components/skill-item/skill-item.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ExperiencesComponent } from './components/experiences/experiences.component';
+import { ExperiencesItemComponent } from './components/experiences/components/experiences-item/experiences-item.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { ExperiencesComponent } from './components/experiences/experiences.compo
     SkillsComponent,
     SkillItemComponent,
     FooterComponent,
-    ExperiencesComponent
+    ExperiencesComponent,
+    ExperiencesItemComponent
   ],
   imports: [
     BrowserModule
